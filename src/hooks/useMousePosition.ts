73a21efc8995d16b4dac147fde94826018594087
nico <410827024@qq.com>
2020-10-19 00:50:28 +0800

    改造成对象方式 reactive toRefs

--- src/hooks/useMousePosition.ts
+++ src/hooks/useMousePosition.ts
@@ -1,13 +1,20 @@
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, reactive, toRefs, onMounted, onUnmounted } from 'vue'
 // 其实就是一个函数， 其他地方导入这个函数就可以了
 // 逻辑脱离了组件 跟组件没有关系
 // 不需要模板
 function useMousePosition() {
-  const x = ref(0)
-  const y = ref(0)
+  const data = reactive({
+    x: 10,
+    y: 0
+  })
+  // const x = ref(0)
+  // const y = ref(0)
+
   const updateMouse = (e: MouseEvent) => {
-    x.value = e.pageX
-    y.value = e.pageY
+    // x.value = e.pageX
+    // y.value = e.pageY
+    data.x = e.pageX
+    data.y = e.pageY
   }
   onMounted(() => {
     // document.addEventListener('click', updateMouse)
@@ -17,7 +24,7 @@ function useMousePosition() {
   onUnmounted(() => {
     document.removeEventListener('click', updateMouse)
   })
-  return {x, y}
+  return toRefs(data)
 }
 
 export default useMousePosition
